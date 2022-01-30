5c6c0bafd7d83789d194dfd8deeaff7daecf141a
Chris Wininger <chris.wininger@gmail.com>
2022-01-30 12:54:15 -0500

    trying to fix the dart

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -22,8 +22,8 @@ flutter pub get #(https://stackoverflow.com/questions/64046233/issue-building-fl
 
 echo "Installed flutter to `pwd`/flutter"
 
-flutter build apk --profile -t target # from (https://stackoverflow.com/questions/62720395/flutter-release-apk-error-execution-failed-for-task-applintvitalrelease)
-flutter build apk --release -t target # from (https://stackoverflow.com/questions/64046233/issue-building-flutter-android-apk-with-ms-appcenter-using-flavors)
+flutter build apk --profile -t $target # from (https://stackoverflow.com/questions/62720395/flutter-release-apk-error-execution-failed-for-task-applintvitalrelease)
+flutter build apk --release -t $target # from (https://stackoverflow.com/questions/64046233/issue-building-flutter-android-apk-with-ms-appcenter-using-flavors)
 
 #copy the APK where AppCenter will find it
 mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-release.apk $_
