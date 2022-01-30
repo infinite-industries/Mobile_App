9cdb6e58bb7cb40ce9a1d820bdde872a23735e63
Chris Wininger <chris.wininger@gmail.com>
2022-01-30 12:42:52 -0500

    much script wow

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -9,15 +9,21 @@ set -e
 set -x
 
 cd ..
-git clone -b master https://github.com/flutter/flutter.git
+git clone -b stable https://github.com/flutter/flutter.git
 export PATH=`pwd`/flutter/bin:$PATH
+
+# ==== from stackflow (https://stackoverflow.com/questions/64046233/issue-building-flutter-android-apk-with-ms-appcenter-using-flavors) ====
+target="lib/main.dart"
+# ====
 
 flutter channel stable
 flutter doctor
+flutter pub get #(https://stackoverflow.com/questions/64046233/issue-building-flutter-android-apk-with-ms-appcenter-using-flavors)
 
 echo "Installed flutter to `pwd`/flutter"
 
-flutter build apk --release
+flutter build apk --profile -t target # from (https://stackoverflow.com/questions/62720395/flutter-release-apk-error-execution-failed-for-task-applintvitalrelease)
+flutter build apk --release -t target # from (https://stackoverflow.com/questions/64046233/issue-building-flutter-android-apk-with-ms-appcenter-using-flavors)
 
 #copy the APK where AppCenter will find it
 mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-release.apk $_
